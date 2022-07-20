fcefbaeee5677634f9eb32a26f8eb1c6c3354f3b
tmerritt56 <104723943+tmerritt56@users.noreply.github.com>
2022-07-20 17:50:16 -0500

    added head section

--- Assets/script.js
+++ Assets/script.js
@@ -1,1 +1,17 @@
-var APIWeather = "85908a6f179294682198f458c3bfa8f3";
+var responseText = document.getElementById('response-text');
+var badRequestUrl = "api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=858c53e89b222f3c6abb400167b0fee4"
+
+function getApi(request) {
+    fetch(request) 
+    .then(function(response) {
+        console.log(response.status);
+       return response.json();
+
+    })
+}
+
+
+
+getApi(badRequestUrl);
+
+
